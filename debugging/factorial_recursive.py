9cb89c0a1c304410c87c07f9377a27b05f6b7a8d
SaidZaier <8770@holbertonstudents.com>
2024-05-06 12:31:53 +0200

    add task 1 to 6, using chatgpt to debugg

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+
+def factorial(n):
+    """
+    Calculates the factorial of a given number recursively.
+
+    Parameters:
+        n (int): The number whose factorial is to be calculated.
+
+    Returns:
+        int: The factorial of the input number.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+f = factorial(int(sys.argv[1]))
+print(f)
